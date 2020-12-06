85ed6c93b4ebad7148f1c079e73dfcfbdad77b93
Philippe Jabet <philippe.jabet@neo-soft.fr>
2020-12-06 09:39:00 +0100

    first commit

---
+++ 2020/day6.js
@@ -0,0 +1,11 @@
+;(function () {
+    const boardingPass = [
+    ];
+    window.onload = function () {
+        console.log('day6.js');
+        let p1 = 0;
+        let p2 = 0;
+        console.log('Part 1 : ' + p1);
+        console.log('Part 2 : ' + p2);
+    };
+})();
