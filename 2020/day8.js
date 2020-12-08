39241d24388c923c8ee383e4cf205ffd3acf86a2
Philippe <philippe.jabet@neo-soft>
2020-12-08 17:08:37 +0100

    day8#part2

--- 2020/day8.js
+++ 2020/day8.js
@@ -1,6 +1,6 @@
 ;(function () {
 
-    const puzzleSolving = (puzzleInput) => {
+    const part1 = (puzzleInput) => {
         const instructions = puzzleInput.replace(/\n/g, '_').split('_');
         let ranInstructions = [];
         let l = 0
@@ -22,8 +22,60 @@
                 ++l;
             }
         }
-        displayPart1(acc);
-        displayPart2(0);
+        return acc;
+    }
+
+    const hasNotInfiniteLoop = (puzzleInput) => {
+        let ranInstructions = [];
+        let l = 0
+        let acc = 0;
+        while (ranInstructions.indexOf(l) === -1) {
+            let instruction = puzzleInput[l];
+            let instructionSplit = instruction.split(' ');
+            const operation = instructionSplit.shift();
+            const argument = instructionSplit.shift().split('');
+            const sign = argument.shift();
+            const numb = parseInt(argument.join(''),10);
+            ranInstructions.push(l);
+            if (operation === 'acc') {
+                acc = sign === '+' ? acc + numb: acc - numb;
+            }
+            if (operation === 'jmp') {
+                l = sign === '+' ? l + numb: l - numb;
+            }else{
+                ++l;
+            }
+            if (l === puzzleInput.length) {
+                return acc;
+            }
+        }
+        return false;
+    }
+
+    const part2 = (puzzleInput) => {
+        const instructions = puzzleInput.replace(/\n/g, '_').split('_');
+        for (let i=0;i<puzzleInput.length; ++i){
+            let puzzleInputUpdated = instructions;
+            let instruction = instructions[i];
+            let instructionSplit = instruction.split(' ');
+            const operation = instructionSplit.shift();
+            const argument = instructionSplit.shift().split('');
+            const sign = argument.shift();
+            const numb = parseInt(argument.join(''),10);
+            if (operation === "jmp" || (operation === "nop" && numb !== 0) ) {
+                puzzleInputUpdated[i] = (operation === "nop") ? 'jmp '+sign+numb : 'nop '+sign+numb;
+                if (hasNotInfiniteLoop(puzzleInputUpdated)) {
+                    return hasNotInfiniteLoop(puzzleInputUpdated);
+                }
+                puzzleInputUpdated[i] = instruction;
+            }
+        }
+        return 'shit';
+    }
+
+    const puzzleSolving = (puzzleInput) => {
+        displayPart1(part1(puzzleInput));
+        displayPart2(part2(puzzleInput));
     };
 
     window.onload = function () {
