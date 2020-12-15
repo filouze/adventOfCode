082d22733e5630d26a741b81dd26dfcb09a68f0b
Philippe <philippe.jabet@neo-soft>
2020-12-15 19:11:57 +0100

    day15#part2

--- 2020/day15.js
+++ 2020/day15.js
@@ -1,17 +1,33 @@
 const fs = require("fs");
 
+const part = (n, p) => {
+    let last = 0;
+    let next = p.pop();
+    let t = p.length;
+    const end = n - 1;
+    let lastIndexes = [];
+    for (let l = 0;l<t;++l) {
+        lastIndexes[p[l]] = l;
+    }
+    while(t < end) {
+        last = next;
+        if (typeof lastIndexes[last] === 'undefined') {
+            next = 0;
+        }else{
+            next = t - lastIndexes[last];
+        }
+        p.push(last);
+        if (t%100000 === 0){
+            console.log(t);
+        }
+        lastIndexes[last] = t;
+        ++t
+    }
+    return next
+}
+
 fs.readFile(process.argv[2], "utf8", function (err, contents) {
     let p = contents.split(',').map(x=>parseInt(x,10));
-    while(p.length < 2020) {
-        const t = p.length - 1
-        const last = p.pop();
-        const lastIndex = p.lastIndexOf(last);
-        if (p.lastIndexOf(last) === -1) {
-            p.push(last,0);
-        }else{
-            p.push(last,t - lastIndex);
-        }
-    }
-    console.log('#part1 = ',p.pop());
-    console.log('#part2 = ');
+    console.log('#part1 = ',part(2020,p));
+    console.log('#part2 = ',part(30000000,p));
 });
