fdb80a4d5cc4130a25495c370ef11d34cdbeca4a
Philippe <philippe.jabet@neo-soft>
2020-12-15 17:53:45 +0100

    day15#part1

--- 2020/day15.js
+++ 2020/day15.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
+
 fs.readFile(process.argv[2], "utf8", function (err, contents) {
-    let p = contents.split(',');
-    console.log('#part1 = ',p);
+    let p = contents.split(',').map(x=>parseInt(x,10));
+    while(p.length < 2020) {
+        const t = p.length - 1
+        const last = p.pop();
+        const lastIndex = p.lastIndexOf(last);
+        if (p.lastIndexOf(last) === -1) {
+            p.push(last,0);
+        }else{
+            p.push(last,t - lastIndex);
+        }
+    }
+    console.log('#part1 = ',p.pop());
     console.log('#part2 = ');
 });
