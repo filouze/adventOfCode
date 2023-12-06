8cac962df6eb141958be3098aea07aa64b9e72dc
philippe.jabet <philippe.jabet@neosoft.fr>
2023-12-06 06:38:02 +0100

    Part2

--- 2023/day01/day01.js
+++ 2023/day01/day01.js
@@ -1,6 +1,26 @@
 const fs = require("fs");
+const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 const getKey = (line) => {
+        let firstDigit = "one"
+        let lastDigit = "one"
+        let f = line.length
+        let l = 0
+        numbers.forEach((n, i) => {
+            const first = line.indexOf(n)
+            const last = line.lastIndexOf(n)
+            if (first != -1 && first < f) {
+                f = first
+                firstDigit = i
+            }
+            if (last != -1 && last > l) {
+                l = last
+                lastDigit = i
+            }
+        })
+        if (f<l) line = line.replace(numbers[firstDigit],firstDigit)
+        if (l>f) line = line.replace(numbers[lastDigit],lastDigit)
+        console.log(" |-> ",numbers[firstDigit], numbers[lastDigit], line)
         const splitedLine = line.split('')
         const integers = splitedLine.filter((c) => Number.isInteger(parseInt(c,10)) === true)
         if (integers.length === 1) return parseInt(integers + integers, 10)
@@ -12,8 +32,9 @@ fs.readFile(process.argv[2], "utf8", function (err, contents) {
     pi.pop()
     let sum = 0
     pi.forEach((line) => {
-        //console.log(sum)
-        sum += getKey(line)
+        const key = getKey(line)
+        console.log(line, key)
+        sum += key
     })
     console.log("Answer : " + sum);
 });
