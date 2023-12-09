7ad604d27747210fa359c89c83a971c2578918da
philippe.jabet <philippe.jabet@neosoft.fr>
2023-12-09 18:02:07 +0100

    Day04 Part2

--- 2023/day04/day04.js
+++ 2023/day04/day04.js
@@ -19,20 +19,38 @@ const getCardValue = (nums, wins) => {
     return 0
 }
 
+const getWonCopy = (nums, wins) => {
+    let winnings = []
+    nums.forEach((n) => {
+        if (wins.indexOf(n) !== -1) winnings.push('0')
+    })
+    return winnings.length
+}
+
 fs.readFile(process.argv[2], "utf8", function (err, contents) {
     let pi = contents.split('\n')
     pi.pop()
     let sum1 = 0
     let sum2 = 0
+    let instances = []
+    let wonCopies = []
     pi.forEach((l) => {
         const cardId = getCardId(l)
         const winningNums = getWinningNums(l)
         const cardNums = getCardNums(l)
         const cardValue = getCardValue(cardNums, winningNums)
         sum1 += cardValue
-        //console.log(l)
-        //console.log("#" + cardId, winningNums, cardNums, cardValue)
+        const wonCopy = getWonCopy(cardNums, winningNums)
+        wonCopies.push(wonCopy)
+        instances.push(1)
+    })
+    wonCopies.forEach((wc,i) => {
+        if (wc) {
+            for (let j = i + 1; j< i + 1 + wc; j++) {
+                instances[j] = instances[j] + instances[i]
+            }
+        }
     })
     console.log("Answer #1 : " + sum1)
-    console.log("Answer #2 : " + sum2)
+    console.log("Answer #2 : " + instances.reduce((a,c) => c + a, 0))
 });
